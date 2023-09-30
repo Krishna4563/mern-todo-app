b43c69a948e56b837fb04bcb6542c99d2b1b4f65
Krishna Keshava Das <daskrishna0902@gmail.com>
2023-09-30 18:54:34 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config();
+const cors= require('cors');
+
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 3002;
+
+app.use(cors());
+
+const TodoItemRoutes = require('./routes/todoroute');
+
+
+mongoose.connect(process.env.DB_CONNECT)
+.then(()=> console.log("Database Connected !"))
+.catch(err=> console.log(err));
+
+
+
+
+
+
+app.use('/',TodoItemRoutes);
+
+app.listen(PORT,()=>console.log("Server Connected"));
